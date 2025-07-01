079811f8783e11cfc7e1070a60dc797de7cebd36
InsiderAnh <leo_yt_29@hotmail.com>
2025-07-01 15:05:21 -0500

    add abstract Animal class with essential methods

---
+++ src/main/java/io/github/insideranh/clase8/Animal.java
@@ -0,0 +1,13 @@
+package io.github.insideranh.clase8;
+
+public abstract class Animal {
+
+    public abstract void sonido();
+
+    public abstract void comer();
+
+    public abstract void beber();
+
+    public abstract boolean esDomestico();
+
+}
